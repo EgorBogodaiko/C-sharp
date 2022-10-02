4829eaa5ef8145a51b26204befc6de7a8b5a1d56
Egor <1begosha@gmail.com>
2022-10-02 12:27:39 +0300

    Add task02

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -5,4 +5,13 @@
 // a = 5; b = 7 -> max = 7
 // a = 2; b = 10 -> max = 10
 // a = -9; b = -3 -> max = -3
-
+Console.WriteLine("Введите первое число: ");
+int first = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int second = Convert.ToInt32(Console.ReadLine());
+int max = first;
+if (first < second)
+{
+    max = second;
+}
+Console.WriteLine($"максимальное число: {max}");
