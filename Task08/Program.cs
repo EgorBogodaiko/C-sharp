893227d2c1ace81113db7944598ffc937e2efb87
Egor <1begosha@gmail.com>
2022-10-02 13:00:01 +0300

    Add task06

---
+++ Task08/Program.cs
@@ -0,0 +1,22 @@
+﻿//  Напишите программу, которая на вход принимает
+//  число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите целое число, желательно положительное: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number <= 1)
+{
+    Console.WriteLine("Введённое число меньше 2, поэтому чётных чисел от 1 до N не существует");
+}
+else
+{
+    Console.WriteLine($"Чётные числа, которые есть от 1 до {number}: ");
+
+    for (int counter = 2; counter <= number; counter = counter + 2)
+    {
+        Console.WriteLine(counter);
+    }
+}
