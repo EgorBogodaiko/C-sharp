cdaeeb7b1d63e53f20a7a9d25a3df24b79e8f31b
Egor <1begosha@gmail.com>
2022-10-10 20:06:23 +0300

    recreated

---
+++ Task10-1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход  
+//трёхзначное число и на выходе показывает вторую цифру этого числа. 
+ 
+// 456 -> 5 
+// 782 -> 8 
+// 918 -> 1 
+ 
+Console.WriteLine("Введите целое трёхзначное число: "); 
+int number = Convert.ToInt32(Console.ReadLine()); 
+ 
+int firstDigit = number/100; 
+int twoDigitsNumber = number - 100*firstDigit; 
+int secondDigit = twoDigitsNumber/10; 
+ 
+Console.WriteLine($"Второе число={secondDigit}");
