3f4b2bdc41de752786c27d53c0d88d8562261f33
Egor <1begosha@gmail.com>
2022-10-12 21:42:52 +0300

    finished19

--- task19/Program.cs
+++ task19/Program.cs
@@ -1,39 +1,52 @@
 ﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 // Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
-
 // 14212 -> нет
-
 // 12821 -> да
-
 // 23432 -> да
-int[] digits = new int(100);
+
+int[] digits = new int[100];
 string numstring;
-int numint = default;
-int size = default;
+int mis = 0;
+
 Console.WriteLine("Введите число (до 100 символов) для проверки на палидромность:");
-Console.ReadLine(numstring);
-numint = Convert.ToInt32(numstring);
-
-
+numstring = Console.ReadLine();
+int size = numstring.Length;
 for (int i = 0; i < size; i++)
 {
-    digits[i] = getdigit(numint, i);
+    digits[i] = getdigit(numstring, i + 1);
+}
+
+for (int i = 0; i < (size / 2); i++)
+{
+    if ((digits[i]) != (digits[size - i-1]))
+    {
+        mis++;
+    }
+}
+
+if (mis > 0)
+{
+    Console.WriteLine("Не палиндром");
+}
+else
+{
+    Console.WriteLine("Палиндром");
 }
 
 
-
-int getdigit(string numba, int count)
+int getdigit(string numba, int count) 
 {
-    int size = numstring.Length;
+    int sizenum = numstring.Length; 
     int intnumber = Convert.ToInt32(numba);
     int ii = 0;
+    int thisDigit = default;
     while (ii < count)
     {
-        int thisDigit = intnumber / Convert.ToInt32(Math.Pow(10, (size - 1)));
-        intnumber = intnumber - thisDigit * Convert.ToInt32(Math.Pow(10, (size - 1)));
-        i++;
-        size--;
+        thisDigit = intnumber / Convert.ToInt32(Math.Pow(10, (sizenum - 1)));         
+        intnumber = intnumber - thisDigit * Convert.ToInt32(Math.Pow(10, (sizenum - 1)));
+        ii++;
+        sizenum--;
     }
-
     return thisDigit;
 }
+
