bd2045833e404314a016af774f6c05ee1e63aab8
Egor <1begosha@gmail.com>
2022-10-13 19:43:53 +0300

    finish25

--- task25/Program.cs
+++ task25/Program.cs
@@ -3,27 +3,29 @@
 // 3, 5 -> 243 (3⁵)
 
 // 2, 4 -> 16
-Console.Writeline("Введите целое число, которое будет основанием степени:");
-int a = Convert.ToInt32(Console.Readline());
-Console.Writeline("Введите целое число, которое будет показателем степени:");
-int b = Convert.ToInt32(Console.Readline());
-int expo=default;
-Console.Writeline($"Число {a}  в степени {b} = ");
-if (b == 0) Console.Writeline("1");
-if (b == 1) Console.Writeline("1");
-if ((b!=0)&&(b!=1))
+Console.WriteLine("Введите целое число, которое будет основанием степени:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое число, которое будет показателем степени:");
+int b = Convert.ToInt32(Console.ReadLine());
+double expo=default;
+Console.WriteLine($"Число {a}  в степени {b} = ");
+
+if ((b == 0)&&(a!=0)) Console.WriteLine("1");
+if (a == 1) Console.WriteLine("1");
+
+if ((b!=0)&&(a!=1))
 {
     expo = Expo(a,b);
-    Console.Writeline($"{expo}");
+    Console.WriteLine($"{expo}");
 }
 
-
-int Expo(int numA, int numB)
+double Expo(int numA, int numB)
 {
-    int result = numA;
-    for (int i = 0; i < numB; i++)
+    double result = numA;
+    for (int i = 1; i < Math.Abs(numB); i++)
     {
         result = numA * result;
     }
+    if (numB<0) result=1/result;
     return result;
 }
