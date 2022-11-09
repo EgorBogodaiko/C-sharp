f93743657bb370894b0ef36173c32f7070f4fa75
Egor <1begosha@gmail.com>
2022-11-09 20:45:54 +0300

    task54 finished

--- task54/Program.cs
+++ task54/Program.cs
@@ -7,4 +7,60 @@
 // 7 4 2 1
 // 9 5 3 2
 // 8 4 4 2
+System.Console.Write("Введите количество строк массива: ");
+int k = System.Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите количество столбцов массива: ");
+int m = System.Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[k, m];
+FillRndMatrix2D(array, 1, 10);
+PrintArrayTwo(array);
+SortRowsArr(array);
+System.Console.WriteLine("Отсортировали содержимое строк по убыванию:");
+PrintArrayTwo(array);
 
+void FillRndMatrix2D(int[,] arr, int mins, int maxs)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = rnd.Next(mins, maxs + 1);
+        }
+    }
+}
+
+void PrintArrayTwo(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (j < arr.GetLength(1) - 1) Console.Write($"{arr[i, j],4}, ");
+            else Console.Write($"{arr[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+void SortRowsArr(int[,] arr)
+{int temp =arr[0,0];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1)-1; j++)
+        {
+            for (int k = 0; k < arr.GetLength(1)-1; k++)
+            {
+                if (arr[i, k] < arr[i, k + 1])
+                {
+                    temp = arr[i, k];
+                    arr[i, k] = arr[i, k + 1];
+                    arr[i, k + 1] = temp;
+                }
+
+            }
+
+        }
+    }
+}
